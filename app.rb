777ffe2739025b8fb0eee7357b7e99ab22e81a30
saasbook <saasbook@saasbook.(none)>
2015-02-09 22:40:13 -0400

    first commit

--- app.rb
+++ app.rb
@@ -39,7 +39,17 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
+    
     ### YOUR CODE HERE ###
+    begin 
+      result = @game.guess(letter)
+      if (result == false)
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+
     redirect '/show'
   end
   
@@ -50,17 +60,32 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    
+    if @game.check_win_or_lose == "win"
+      redirect '/win'
+    elsif @game.check_win_or_lose == "lose"
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == "win"
+      erb :win 
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == "lose"
+      erb :lose 
+    else
+      redirect '/show'
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,6 +5,53 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def initialize(new_word)
+  	@word = new_word
+  	@guesses = ""
+  	@wrong_guesses = ""
+  end
+
+  def guess(letter)
+  	if (letter == nil) or (letter == "") or ((/[[:alpha:]]/ =~ letter) == nil)
+  		raise ArgumentError.new("Invalid guess.")
+  	elsif @guesses.include?(letter) or @wrong_guesses.include?(letter)
+  		return false
+  	else
+  		#this is a valid letter
+ 		if @word.include?(letter)
+ 			@guesses += letter
+ 		else
+ 			@wrong_guesses += letter
+ 		end
+ 		return true
+ 	end
+  end
+  		
+  def word_with_guesses
+  	result = ""
+  	@word.split("").each do |letter|
+  		if @guesses.include? "#{letter}"
+  			result += "#{letter}"
+  		else
+  			result += "-"
+  		end
+  	end
+  	return result
+  end
+
+  def check_win_or_lose
+  	limit = 7
+  	if word_with_guesses == word
+  		return :win
+  	elsif wrong_guesses.length >= limit
+ 		return :lose
+ 	else
+ 		return :play
+ 	end
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
